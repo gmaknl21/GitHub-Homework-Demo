dff262f16b1815b0a1737788ab9a12d70f4a227e
gmaknl21 <girmaybarakiak21@gmail.com>
2023-01-27 21:07:32 +0100

    function-week3

---
+++ Answer.py
@@ -0,0 +1,6 @@
+# function with models
+from model_operation import * # import my model
+
+print("Answer number 1:", add(1,2))
+
+print("Answer number 2:", sub(2,3))
---
+++ model_operation.py
@@ -0,0 +1,16 @@
+# Additiona
+def add(num1, num2):
+    return num1 + num2
+
+# Subtraction
+def sub(num1, num2):
+    return num1 - num2
+
+# Multiplication
+def mul(num1, num2):
+    return num1 * num2
+
+# Divide
+def div(num1, num2):
+    return  num1 / num2
+
